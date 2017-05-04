8a355056248c0676e3d77e7b05ff57b1c3bfba14
marcin <mwloczko@gmail.com>
2017-05-04 12:16:03 +0200

    created project

---
+++ QuizApp/QuizApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Marcin on 04.05.2017.
+//  Copyright © 2017 Marcin Włoczko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
