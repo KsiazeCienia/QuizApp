200478909d6ef7249da77e90175dfba337054310
marcin <mwloczko@gmail.com>
2017-05-04 12:19:06 +0200

    added quiz file

---
+++ QuizApp/QuizApp/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Marcin on 04.05.2017.
+//  Copyright © 2017 Marcin Włoczko. All rights reserved.
+//
+
+import UIKit
+
+class Question: NSObject {
+
+}
