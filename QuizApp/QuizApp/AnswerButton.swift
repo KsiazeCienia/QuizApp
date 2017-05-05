bb05c3a0cf576e75cb052f3822003bed20635b56
marcin <mwloczko@gmail.com>
2017-05-05 20:36:14 +0200

    added answerButton class

---
+++ QuizApp/QuizApp/AnswerButton.swift
@@ -0,0 +1,45 @@
+//
+//  AnswerButton.swift
+//  QuizApp
+//
+//  Created by Marcin on 05.05.2017.
+//  Copyright © 2017 Marcin Włoczko. All rights reserved.
+//
+
+import UIKit
+
+class AnswerButton: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    var numberStackView = UIStackView()
+    var answerStackView = UIStackView()
+    
+    var numberLabel = UILabel()
+    var answerLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.axis = .horizontal
+        
+        answerStackView.addArrangedSubview(answerLabel)
+        self.addArrangedSubview(answerStackView)
+        
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setAnswerLabel(answerText: String) {
+        answerLabel.text = answerText
+    }
+
+}
--- QuizApp/QuizApp/ViewController.swift
+++ QuizApp/QuizApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var answersStackView: UIStackView!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    var currentQuestion:Question?
+    
     let model = QuizModel()
     var questions = [Question]()
 
@@ -17,11 +22,51 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
          self.questions = model.getQuestions()
+        
+        if (questions.isEmpty) {
+            currentQuestion = questions[0]
+            
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func displayCurrentQuestion() {
+        
+        if let actualCurrentQuestion = currentQuestion {
+            
+            questionLabel.text = actualCurrentQuestion.questionText
+            
+            //create answers buttons
+            for _ in 0...actualCurrentQuestion.answers.count {
+                
+            }
+        }
+    }
+    
+    func createAnswerButtons() {
+        
+        if let actualCurrentQuestion = currentQuestion {
+        
+            for i in 0..<actualCurrentQuestion.answers.count {
+                
+                let answerButton = AnswerButton()
+                
+                answerButton.setAnswerLabel(answerText: actualCurrentQuestion.answers[i])
+                
+                let heightConstraint = NSLayoutConstraint(item: answerButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
+                answerButton.addConstraint(heightConstraint)
+                
+                answersStackView.addArrangedSubview(answerButton)
+                
+            }
+        }
     }
 
 
